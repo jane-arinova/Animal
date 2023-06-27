bb30c40d52758df9b70929fb9d813f641e80b4e3
JazeeraArinova <canarijaz@gmail.com>
2023-06-27 13:35:26 -0400

    added do/while

--- src/Animal/Dog.java
+++ src/Animal/Dog.java
@@ -3,5 +3,8 @@ package Animal;
 public class Dog {
     public static void main(String[] args) {
         System.out.println("woof");
+        do{
+            System.out.println("Love java");
+        }while(true);
     }
 }
