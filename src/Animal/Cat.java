15bd0b20a1a5c4118784eed5dd7a6ba9d246bce3
JazeeraArinova <canarijaz@gmail.com>
2023-06-27 13:18:30 -0400

    updated Cat with reverseString method

--- src/Animal/Cat.java
+++ src/Animal/Cat.java
@@ -2,10 +2,13 @@ package Animal;
 
 public class Cat {
     public static void main(String[] args) {
-
+        System.out.println(reverseString("hubba"));
     }
     public static String reverseString(String string){
         String reverseText = "";
+        for (int i = 0; i < reverseText.length(); i++) {
+            reverseText = string.charAt(i) + reverseText;
+        }
 
         return reverseText;
     }
