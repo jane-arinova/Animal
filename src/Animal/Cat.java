d22a348e513b385fc9e3c5c037089f43be11899f
JazeeraArinova <canarijaz@gmail.com>
2023-06-27 13:14:46 -0400

    Created Cat class

---
+++ src/Animal/Cat.java
@@ -0,0 +1,13 @@
+package Animal;
+
+public class Cat {
+    public static void main(String[] args) {
+
+    }
+    public static String reverseString(String string){
+        String reverseText = "";
+
+        return reverseText;
+    }
+
+}
