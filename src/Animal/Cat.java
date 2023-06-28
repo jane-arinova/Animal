1f63f28c0610395858a4b6e52d3e27178848fa4d
Hidayet BOYA <hdyt293@gmail.com>
2023-06-27 20:15:51 -0600

    added new commend

--- src/Animal/Cat.java
+++ src/Animal/Cat.java
@@ -3,9 +3,6 @@ package Animal;
 public class Cat {
     public static void main(String[] args) {
         System.out.println("meow");
-        System.out.println("hi");
-
         // used sout only
-        System.out.println("Thank you Jazeera");
     }
 }
